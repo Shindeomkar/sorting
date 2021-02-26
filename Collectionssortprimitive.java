63447552d8ec4f88cb774c8ee997be19353de086
omkar shinde <omkar3631@gmail.com>
2021-02-26 14:58:29 +0530

    Add files via upload

---
+++ Collectionssortprimitive.java
@@ -0,0 +1,17 @@
+package Sorting;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Collectionssortprimitive {
+    public static void main(String[] args) {
+        ArrayList<Integer> l1=new ArrayList<>();
+        l1.add(20);
+        l1.add(10);
+        l1.add(39);
+        Collections.sort(l1);
+        System.out.println(l1);
+    }
+}
